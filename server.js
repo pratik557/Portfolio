3ef9c8b000a775fa683768325de0cc7e35e1b70a
Pratik Gupta <81405739+pratik557@users.noreply.github.com>
2022-02-26 21:01:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const dotenv = require("dotenv");
 dotenv.config();
 const connection = require("./connection/connection");
